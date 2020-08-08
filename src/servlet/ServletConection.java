3f1d1e63e947efdb3e88f1e7853b33841bee469d
fleitasnelson1-bootcamp <sonnel59fleitas+bootcamp1@gmail.com>
2020-08-08 10:03:26 -0400

    Se hizo que el servlet login responda con el archivo JSP de login

--- src/servlet/ServletConection.java
+++ src/servlet/ServletConection.java
@@ -1,7 +1,10 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletConection
  */
+@WebServlet("/Login")
 public class ServletConection extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -24,7 +28,8 @@ public class ServletConection extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher loginHtml = request.getRequestDispatcher("/views/login/login.jsp");
+		loginHtml.forward(request,response);
 	}
 
 	/**
