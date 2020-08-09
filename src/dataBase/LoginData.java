dff9130ffb0e4ab7aa1c675f1620210a1ad4d02b
William <necroyang@gmail.com>
2020-08-09 19:48:10 -0400

    Added Conector

--- src/dataBase/LoginData.java
+++ src/dataBase/LoginData.java
@@ -1,5 +1,28 @@
 package dataBase;
 
-public class LoginData {
+import java.sql.*;
 
+import ultil.Conector;
+
+public class LoginData {
+	//inspirado en el video 52
+	public void insert(Conector co, String nick, String password, String salt) {
+		
+		try(Connection con = co.getConector()){
+			
+			if(nick!=null && password!=null && salt !=null) {
+				try(PreparedStatement pstm = con.prepareStatement("INSERT INTO Usuario(Nombre_user,Contraseña_user,Salt_user) values (?,?,?)", new String[] {"Id_user"})){
+					pstm.setString(1, nick);
+					pstm.setString(2, password);
+					pstm.setString(3, salt);
+					pstm.executeQuery();
+				}
+			}
+			
+		}catch (SQLException e) {
+			throw new Error("Error al iniciar el servidor");
+		}
+		
+	}
+	
 }
