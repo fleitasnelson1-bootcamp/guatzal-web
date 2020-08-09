11262340713389c3f6aeb3d9849f6389ea97b56b
fleitasnelson1-bootcamp <sonnel59fleitas+bootcamp1@gmail.com>
2020-08-09 19:22:25 -0400

    Codigo js para probar envio POST de datos de formulario login

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -23,6 +23,17 @@ public class LoginServlet extends HttpServlet {
     }
 
 	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.println(username);
+		response.getWriter().print(username + " - " + password);
+	}
+
+	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
