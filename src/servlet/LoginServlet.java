7acc767b88299015471bcef17be9c59db11a186f
fleitasnelson1-bootcamp <sonnel59fleitas+bootcamp1@gmail.com>
2020-08-09 21:08:34 -0400

    Agregada anotacion para leer FormData en servlet para login

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class loginServlet
  */
 @WebServlet(description = "Servlet for login", urlPatterns = { "/login" })
+@MultipartConfig
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,10 +29,7 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		System.out.println(username);
-		response.getWriter().print(username + " - " + password);
+		response.getWriter().print(request.getParameter("username") + " - " + request.getParameter("password"));
 	}
 
 	/**
